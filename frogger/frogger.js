42892d431dda76b9d9f83fb8fca054e50fb46588
Omar Guerrero <oguerrero@unal.edu.co>
2026-09-20 16:18:18 -0500

    inicio de proyecto

---
+++ frogger/frogger.js
@@ -0,0 +1,37 @@
+const vel = 1;
+let obstacle; // Declaración de la variable global
+
+function setup() {
+  createCanvas(800, 1000);
+  obstacle = new Obstacle();
+}
+
+function draw() {
+  background(0);
+  obstacle.draw();
+  obstacle.update();
+  //rect(50, 50, 75, 100);
+}
+
+class Obstacle {
+  constructor() {
+    this.xpos = 0;
+    this.vivo = true;
+  }
+
+  draw() {
+    push();
+      noStroke();
+      fill(255, 0, 0);
+      rect(this.xpos, 0, 100, 100);
+    pop();
+  }
+
+  update() {
+    this.xpos = this.xpos + vel; 
+  }
+  
+  alive(){
+    this.vivo = false;
+  }
+}
