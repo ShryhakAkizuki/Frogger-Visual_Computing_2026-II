804dc7d30b6c82872ae6afbd1216402ff704c489
Omar Nicolas Guerrero <64154456+camisabo@users.noreply.github.com>
2026-09-22 18:44:43 +0100

    Refactor Obstacle class and adjust game setup
    Refactor Obstacle class to accept size and position parameters. Update setup and draw functions to accommodate changes.

--- frogger/frogger.js
+++ frogger/frogger.js
@@ -1,37 +1,63 @@
 const vel = 1;
+const lines = 10
+
+
 let obstacle; // Declaración de la variable global
 
 function setup() {
-  createCanvas(800, 1000);
-  obstacle = new Obstacle();
+  createCanvas(800, 900);
+  obstacle = new Obstacle(height/lines,0);
+  
 }
 
 function draw() {
   background(0);
-  obstacle.draw();
-  obstacle.update();
+  if (obstacle.vivo){
+    obstacle.draw();
+    obstacle.update();
+  }
   //rect(50, 50, 75, 100);
 }
 
+//
 class Obstacle {
-  constructor() {
-    this.xpos = 0;
+  constructor(obstacleSize,x) {
+    this.xpos = x;
     this.vivo = true;
+    this.propirtionWith = random(1,1.5);
+    this.obstacleSize = obstacleSize
   }
 
   draw() {
     push();
       noStroke();
       fill(255, 0, 0);
-      rect(this.xpos, 0, 100, 100);
+      rect(this.xpos, 0, this.obstacleSize*this.propirtionWith, this.obstacleSize);
     pop();
   }
 
   update() {
     this.xpos = this.xpos + vel; 
+    if (this.xpos > width){
+      this.alive();
+    }
   }
   
   alive(){
     this.vivo = false;
   }
+}
+
+class obstacleLine {
+  constructor(position, dificulty){
+    this.position = position;
+    this.dificulty =dificulty;
+    this.obstacles = [];
+    //this.appeared = falce;
+  }
+
+  update(){
+    
+  }
+  
 }
